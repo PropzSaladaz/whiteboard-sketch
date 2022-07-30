0fed2025f7812df59a86ccc3922de04c171042c6
Sidnei Teixeira <sidneimartinsteixeira@gmail.com>
2022-07-30 16:25:24 +0100

    added theme feature (#9)

---
+++ src/canvas.js
@@ -0,0 +1,95 @@
+/**
+ * Represents the canvas with all drawings
+ * @attribute canvas - The canvas object itself
+ * @attribute context - The canvas' 2d context
+ * @attribute line - a Line object that draws in the canvas
+ * @attribute largest(X/Y)ValueDrawn - name is self explanatory
+ */
+ class Canvas{
+    constructor(canvas,line=undefined){
+        this.canvas = canvas
+        this.context = canvas.getContext('2d')
+        this.context.imageSmoothingQuality='high'
+        this.line = line
+        this.largestXValueDrawn = 0
+        this.largestYValueDrawn = 0
+        this.backgroundColor = undefined
+    }
+    
+    /**
+     * Draws a line to the mouse's current position
+     * @param {mouse} mouse - contains the current position of the mouse 
+     */
+    drawLine(mouse) {
+        this.line.updateCurrentPos(mouse.currentX, mouse.currentY)
+        this.line.updateLastPos(mouse.lastX, mouse.lastY)
+        this.line.drawLine()
+    }
+
+    /**
+     * Deletes all drawings, and resets the canvas size
+     */
+    clearScreen(){
+        if (this.line) this.context.clearRect(0,0,window.innerWidth, window.innerHeight)
+        this.largestXValueDrawn = 0;
+        this.largestYValueDrawn = 0;
+        this.setCanvasWidth()
+        if (this.backgroundColor) {
+            this.context.fillStyle = this.backgroundColor
+            this.context.fillRect(0, 0, window.innerWidth, window.innerHeight)
+        }
+    }
+
+    /**
+     * Resizes the canvas.
+     * If the user resizes to a point where drawings get off screen
+     * the canvas is not resized to preserve the drawing integrity
+     */
+    resize(){
+        if (innerWidth > this.largestXValueDrawn && 
+            innerHeight > this.largestYValueDrawn &&
+            innerWidth != this.canvas.width && 
+            innerHeight != this.canvas.height) {
+            // copy canvas state to a temporary one
+            var tempCanvas = document.createElement('canvas')
+            tempCanvas.width = window.innerWidth
+            tempCanvas.height = window.innerHeight
+            var tempCtx = tempCanvas.getContext('2d')
+            tempCtx.drawImage(this.canvas, 0, 0)
+
+            // copy back to the original
+            this.setCanvasWidth()
+            this.context.drawImage(tempCanvas, 0, 0)
+        }
+    }
+
+    /**
+     * Updates the limit X and Y positions of the entire drawing
+     * to avoid unwanted canvas resizings that might delete lines
+     * from the drawing
+     * @param {mouse} mouse - contains the mouse's current position 
+     */
+    updateCanvasLimits(mouse){
+        if (mouse.currentX > this.largestXValueDrawn) 
+            this.largestXValueDrawn = mouse.currentX
+        if (mouse.currentY > this.largestYValueDrawn) 
+            this.largestYValueDrawn = mouse.currentY
+    }
+
+    setCanvasWidth(){
+        this.canvas.width = window.innerWidth
+        this.canvas.height = window.innerHeight
+    }
+
+    changeBackgroundColor(color) {
+        this.backgroundColor = color
+        this.context.fillStyle = color
+        this.context.fillRect(0, 0, window.innerWidth, window.innerHeight)
+    }
+
+    setLineColor(color) {
+        this.line.setColor(color);
+    }
+}
+
+export { Canvas }
--- src/ui.js
+++ src/ui.js
@@ -1,96 +1,159 @@
 import { foregroundCanvas , backgroundCanvas } from '/src/foreground-canvas.js'
 
 
-// Color pallet - add new colors to the list
-const colors = [ 
-    'rgba(255, 255, 255, 1)' ,
-    'rgba(255, 242, 129, 1)' ,
-    'rgba(106, 120, 245, 1)'   ,
-    'rgba(236, 117, 139, 1)'   ,
-    'rgba(109, 251, 157, 1)'   ,
-    'rgba(0,   0,   0,   1)'
-]
+const borderColor = "#EDEDED"
+const lightMode = 'L'
+const darkMode  = 'D'
 
-const lineWidthInputSlider = document.querySelector('.line-width')
-const rangeInput = document.querySelector('input[type="range"]')
-const colorPallet = document.querySelector('.color-pallet')
-const colorDefaultSize = 35; // color-pallet squares size in pixels
-const minRadius = 1;
+const colors = {
+    white:     'rgba(255, 255, 255, 1)' ,
+    black:     'rgba(0,   0,   0,   1)' ,
+    yellow:    'rgba(255, 242, 129, 1)' ,
+    blue:      'rgba(106, 120, 245, 1)' ,
+    red:       'rgba(236, 117, 139, 1)' ,
+    green:     'rgba(109, 251, 157, 1)' ,
 
-
-const UI = {
-    isInsideMenu: false,
-    color: "black",
-    lineWidth: minRadius,
-
-    setLineColor: function(color) {
-        this.color = color
-    },
-
-    setLineWidth: function(lineWidth) {
-        this.lineWidth = lineWidth
-    },
-
-    getLineColor: function() {
-        return this.color
-    },
-
-    getLineWidth: function() {
-        return this.lineWidth
-    },
-
-    toggleInsideMenu: function() {
-        this.isInsideMenu = !this.isInsideMenu
-    },
-
+    UIBackgroundLightTheme: 'rgba(255, 255, 255, 0.63)'
 }
 
-// ------------------------ Color Pallet -------------------------------- //
-const borderColor = "#EDEDED"
+class Theme {
+    constructor(name, mainLineColor, eraserColor, uiBackgroundColor, backgroundColor) {
+        this.name = name
+        this.mainLineColor = mainLineColor
+        this.eraserColor = eraserColor
+        this.uiBackgroundColor = uiBackgroundColor
+        this.backgroundColor = backgroundColor
+    }
+}
+
+const themes = {
+    light: new Theme(lightMode, colors.black, colors.white, colors.black, colors.UIBackgroundLightTheme ),
+    dark:  new Theme(darkMode , colors.white, colors.black, colors.white, colors.black )
+}
+
+var uiDOM                  = document.querySelector('.ui') 
+const lineWidthInputSlider = document.querySelector('.line-width')
+const rangeInput           = document.querySelector('input[type="range"]')
+const colorPallet          = document.querySelector('.color-pallet')
+const minLineRadius = 1;
 
 
-colors.forEach((color) => {
-    // Add the collors to the DOM
-    var div = document.createElement('div')
-    div.style.backgroundColor = color
-    /* div.style.width =   `${colorDefaultSize}px`
-    div.style.height =  `${colorDefaultSize}px` */
-    div.style.transition = "all 200ms ease-in-out"
-    div.classList.add("color-btn", "btn-border", "btn-hoverable")
-    colorPallet.appendChild(div)
+class UserInterface {
+    constructor() {
+        this.theme = themes.light
+        this.DOM = uiDOM
+        this.isInsideMenu = false
+        this.lineWidth = minLineRadius
+        this.currentColor = themes.light.mainLineColor
+        this.colorPallet = {mainColor: this.theme.mainLineColor ,
+                            yellow:    colors.yellow ,
+                            blue:      colors.blue ,
+                            red:       colors.red ,
+                            green:     colors.green }
 
+        Object.entries(this.colorPallet).forEach((entryArray) => {
+            var color = entryArray[1]
+            // Add the collors to the DOM
+            var div = document.createElement('div')
+            if (color === this.theme.mainLineColor) {
+                styleSelected(div)
+            }
+            div.style.backgroundColor = color
+            div.style.transition = "all 200ms ease-in-out"
+            div.classList.add("color-btn", "btn-border", "btn-hoverable")
+            colorPallet.appendChild(div)
+        
+        
+            // add event listeners to each to change line color if needed
+            div.addEventListener('click', () => {
+                foregroundCanvas.line.setColor(div.style.backgroundColor)
+                this.setCurrentColor(div.style.backgroundColor)
+        
+                var colors = [...colorPallet.children]
+                colors.forEach( (color) => { // reset all colors' border
+                    color.style.borderColor =  borderColor
+                    color.style.borderRadius = "30%"
+                    color.style.borderWidth = "2px"
+                })
+                // put border only on the clicked one
+                styleSelected(div)
+        
+            })
 
-    // To change line color
-    div.addEventListener('click', () => {
-        foregroundCanvas.line.setColor(div.style.backgroundColor)
-
-        var colors = [...colorPallet.children]
-        colors.forEach( (color) => { // reset all colors' border
-            color.style.borderColor =  borderColor
-            color.style.borderRadius = "30%"
-            color.style.borderWidth = "2px"
+            function styleSelected(selectedDiv) {
+                selectedDiv.style.borderWidth = "8px"
+                selectedDiv.style.borderColor = "#6A78F5"
+                selectedDiv.style.borderRadius = "50%"
+            }
+        
         })
-        // put border only on the clicked one
-        div.style.borderWidth = "8px"
-        div.style.borderColor = "#6A78F5"
-        div.style.borderRadius = "50%"
 
-    })
+        
+    }
 
-})
+    setCurrentColor(color) { 
+        this.currentColor = color
+    }
+
+    setLineWidth(lineWidth) {
+        this.lineWidth = lineWidth
+    }
+
+    getLineColor() {
+        return this.currentColor
+    }
+
+    getLineWidth() {
+        return this.lineWidth
+    }
+
+    toggleInsideMenu() {
+        this.isInsideMenu = !this.isInsideMenu
+    }
+
+    toggleTheme(){
+        console.log(this.currentColor)
+        console.log(this.theme.mainLineColor);
+        const isUsingMainColor = (this.currentColor === this.theme.mainLineColor);
+        this.theme = (this.theme.name === lightMode) ? themes.dark : themes.light;
+        if (isUsingMainColor) {
+            this.setCurrentColor(this.theme.mainLineColor);
+            foregroundCanvas.setLineColor(this.currentColor);
+        }
+        colorPallet.mainColor = this.theme.mainLineColor;
+
+        document.querySelector('body').style.backgroundColor = this.theme.backgroundColor;
+        document.querySelector('.color-btn').style.backgroundColor = this.theme.mainLineColor;
+    }
+
+    eventLineWidthChanged() {
+        //this.dom.dispatchEvent(lineWidthChange)
+    }
+
+    clearScreen() {
+        foregroundCanvas.clearScreen()
+        foregroundCanvas.setLineColor(this.currentColor);
+    }
+}
+
+
+const UI = new UserInterface()
+// ------------------------ Color Pallet -------------------------------- //
 
 // ------------------------ LINE WIDTH --------------------------- //
+const lineWidthChange = new Event('lineWidthChange')
 
-lineWidthInputSlider.addEventListener('change', function() {
-    foregroundCanvas.line.setRadius(minRadius + (lineWidthInputSlider.value/25));
-})
+lineWidthInputSlider.addEventListener('change', () =>
+foregroundCanvas.line.setRadius(minLineRadius + (lineWidthInputSlider.value/25)) )
 
 
 // ------------------------ CLEAR SCREEN --------------------------- //
 
-document.querySelector('.clear-screen').addEventListener('mousedown', () => {
-    foregroundCanvas.clearScreen()
-/*     backgroundCanvas.clearScreen() */
+document.querySelector('.clear-screen').addEventListener('mousedown', () => UI.clearScreen())
+
+document.querySelector('.theme').addEventListener('click', () => {
+    UI.toggleTheme();
+    UI.clearScreen();
 })
 
 document.querySelector('.ui').addEventListener('mousedown', () => {
